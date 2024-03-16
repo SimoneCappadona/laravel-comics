141d57109c56ea210ee7dfb01c5c346a3944da07
SimoneCappadona <simocappa95@gmail.com>
2024-03-16 11:07:34 +0100

    edit add structure

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@vite('resources/js/app.js')
+<main>
+    <div class="container d-flex align-items-center justify-content-between py-5">
+        <div id="logo">
+            <img src="{{Vite::asset('images/dc-logo.png')}}" alt="">
+        </div>
+        <div id="header-navbar">
+            <ul>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">CHARACTERS</a>
+            </li>       
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">COMICS</a>
+            </li>       
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">MOVIES</a>
+            </li>       
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">TV</a>
+            </li>       
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">GAMES</a>
+            </li>       
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">COLLECTIBLES</a>
+            </li>       
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">VIDEOS</a>
+            </li>       
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">FANS</a>
+            </li>       
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">NEWS</a>
+            </li>       
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">SHOP</a>
+            </li>       
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Shop
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Comics</a></li>
+                <li><a class="dropdown-item" href="#">Figures</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Gifts</a></li>
+              </ul>
+            </li>
     
+          </ul>
+    
+        </div>
+      </div>
+    </nav>
+    
+        </div>
+    
+        <div id="header-searchbar">
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+
 </body>
+
 </html>
