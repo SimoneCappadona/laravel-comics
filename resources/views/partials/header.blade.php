a961c61e9670f6f32392ddfc999ed080954cafcf
SimoneCappadona <simocappa95@gmail.com>
2024-03-18 14:54:32 +0100

    edit

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,7 @@
 @vite('resources/js/app.js')
+<header class="d-flex flex-row-reverse gap-3">
+  <p class="text-light fw-medium me-5">ADDITIONAL DC SITES</p><p class="text-light fw-medium me-5">DC POWER VISA &#174;</p> 
+</header>
 <main>
     <div class="container d-flex align-items-center justify-content-between py-5">
         <div id="logo">
@@ -9,7 +12,7 @@
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 fw-medium">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">CHARACTERS</a>
             </li>       
@@ -36,13 +39,10 @@
             </li>       
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">NEWS</a>
-            </li>       
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">SHOP</a>
-            </li>       
+            </li>          
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Shop
+                SHOP
               </a>
               <ul class="dropdown-menu">
                 <li><a class="dropdown-item" href="#">Comics</a></li>
@@ -57,15 +57,16 @@
         </div>
       </div>
     </nav>
-    
         </div>
-    
         <div id="header-searchbar">
             <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
         </div>
+    </div>
+    <div class="jumbotron">
+      <img src="{{Vite::asset('images/jumbotron.jpg')}}" alt="" class="img-fluid jumbo">
     </div>
 
 </body>
